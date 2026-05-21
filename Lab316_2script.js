7ecc8e9f129fb256612aa1de6310ab051a90285d
Sammy65433 <samiamlistening@gmail.com>
2026-05-21 16:51:02 -0400

    ALAB 316.2.1: Interacting with the Browser Update 2

--- Lab316_2script.js
+++ Lab316_2script.js
@@ -24,30 +24,73 @@ for (let i = 1; i <= 100; i++) {
 // Use DOM manipulation to give a visual indication of the game's
 // progress in some way.
 
+const secretnumber = Math.floor(Math.random() * 100) + 1;
+// console.log(secretnumber);
+
+let numOfGuesses = 10;
+
+
 // Prompt to open guessing a number
-let userinput = window.prompt("guess a number between 1 and 100");
+const guess = Number(prompt("guess a number between 1 and 100"));
+
+if (guess === secretnumber) {
+    alert(`The number is ${guess}. You win!`)
+} else if (guess < secretnumber) {
+    alert(`The number is ${guess} is too low! Guess again!`)
+
+} else {
+    alert(`The number is ${guess} is too High! Guess again!`)
+}
+
+
+// Create the game loop
+while(numOfGuesses >= 0) {
+// console.log(numOfGuesses)
+const guess = Number(prompt("guess a number between 1 and 100"));
+
+if (guess === secretnumber) {
+    alert(`The number is ${guess}. You win!`)
+    break;
+
+} else if (guess < secretnumber) {
+    alert(`The number is ${guess} is too low! Guess again! \n ${numOfGuesses} guesses remaining`)
+
+} else {
+    alert(`The number is ${guess} is too High! Guess again! \n ${numOfGuesses} guesses remaining`)
+}
+// check if their at zero guesses 
+if (numOfGuesses === 0) {
+    alert('No More Guesses. You Lose!')
+}
+
+    // decrement number of guess to break the loop
+    numOfGuesses--
+}
+
+    // if (guess < secretnumber)
+
 
 // Pick a number between 1 to 100
-// let secretnumber = Math.floor(Math.random() * 100) + 1;
 
-let secretnumber = 25
-// let guessleft = 10
-// while (guessleft > 0) {
-//     let userinput = window.prompt(`Attempts Left: ${guessleft}`);
-//
-for (let i = 3; i >= 0; i--) {
-    window.prompt(`Attempts Left: ${i}`);
-    userinput = Number(userinput);
 
-    if (userinput === secretnumber) {
-        window.alert("Correct, You Guess the Number");
-        break;
-    } else if (userinput < secretnumber) {
-        window.alert("Too Low!!!!");
-    } else if (userinput > secretnumber){
-        window.alert("Too High!!!!");
-    }
-}
+// let secretnumber = 25
+// // let guessleft = 10
+// // while (guessleft > 0) {
+// //     let userinput = window.prompt(`Attempts Left: ${guessleft}`);
+// //
+// for (let i = 3; i >= 0; i--) {
+//     window.prompt(`Attempts Left: ${i}`);
+//     userinput = Number(userinput);
+
+//     if (userinput === secretnumber) {
+//         window.alert("Correct, You Guess the Number");
+//         break;
+//     } else if (userinput < secretnumber) {
+//         window.alert("Too Low!!!!");
+//     } else if (userinput > secretnumber){
+//         window.alert("Too High!!!!");
+//     }
+// }
 // guessleft --
 
 // if less than secret number
